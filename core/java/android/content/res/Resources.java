1fb9515e457bf07d70296778127e3efbf01b44bc
Andy Mast <andy@cyngn.com>
2013-12-09 15:45:33 -0800

    Remove T-mobile items not used by new theme engine
    Change-Id: I1e9c14e63ec25fdff61cfd2b7b8a05b1e488e558
    
    Conflicts:
    	core/java/android/app/ResourcesManager.java

--- core/java/android/content/res/Resources.java
+++ core/java/android/content/res/Resources.java
@@ -2056,7 +2056,7 @@ public class Resources {
             mAssets.recreateStringBlocks();
         }
     }
- 
+
     /*package*/ Drawable loadDrawable(TypedValue value, int id)
             throws NotFoundException {
 
